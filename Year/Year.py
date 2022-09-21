085f1d6c4ccbce01837ee6c04f4da414ea4c4046
SYOTOS <medvezenokb@gmail.com>
2022-09-21 13:57:15 +0300

    Added function for knowing is year leap

--- Year/Year.py
+++ Year/Year.py
@@ -1,1 +1,8 @@
 
+
+def is_year_leap(Year):
+      if f"Year % 4 == 0 and Year % 100 != 0 or Year % 400 == 0" :
+        print("True")
+      else: print("False")
+
+is_year_leap(input("Enter year: "))
